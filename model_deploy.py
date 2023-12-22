82598e841ef9465ae87ff26ade36a448a2a554c6
phuongnvp <55146498+phuongnvp@users.noreply.github.com>
2023-12-22 16:10:23 +0700

    Add files via upload

--- model_deploy.py
+++ model_deploy.py
@@ -7,11 +7,70 @@ import numpy as np
 import pandas as pd               
 import matplotlib.pyplot as plt   
 import seaborn as sns
-from rdkit.Chem import Descriptors
+from rdkit.Chem import Draw, Descriptors, rdqueries
 from rdkit import RDLogger  
 RDLogger.DisableLog('rdApp.*')  
 from rdkit import Chem
 import random
+
+#%% Find function group
+def identify_functional_groups(smiles, functional_groups):
+    """
+    Identify functional groups in a molecule using SMILES representation.
+
+    Args:
+        smiles (str): SMILES representation of the molecule.
+        functional_groups (dict): A dictionary of functional groups with SMARTS patterns.
+
+    Returns:
+        list: List of functional groups found in the molecule.
+    """
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return []
+
+    found_groups = []
+    for group, smarts_pattern in functional_groups.items():
+        pattern = Chem.MolFromSmarts(smarts_pattern)
+        if mol.HasSubstructMatch(pattern):
+            found_groups.append(group)
+
+    return found_groups
+
+# Define a dictionary of functional groups with SMARTS patterns
+functional_groups = {
+    'oxidizing agent': '[$([O]=[O]),$([O-][O+]=[O]),$([OX2,OX1-][OX2,OX1-]),$([Mn](=[OX1])(=[OX1])(=[OX1])[OX1H0-,OH]),$([Cr](=[OX1])(=[OX1])[O])]',
+    'phenolic hydroxyl': '[c;!$(C=O)][OH]',
+    'primary amine': '[NX3;H2;!$(NC=O);!$(NS=O);!$(N=O);!$(N=S);!$(NC=N)]',
+    'secondary amine': '[NX3;H1;!$(NC=O);!$(NS=O);!$(N=O);!$(N=S);!$(NC=N)]',
+    'tertiary amine': '[NX3;H0;!$(NC=O);!$(NS=O);!$(N=O);!$(N=S);!$(NC=N)]',
+    'sulfide': '[SX2;!$(S=O)]',
+    'double bond': '[CX3]=[CX3]',
+    'aldehyde': '[CX3H1](=O)[#6]',
+    'keton': '[#6](=O)[#6]',
+    'carbonamide': '[NX3][CX3](=[OX1])',
+    'sulfonamide': '[$([#16X4]([NX3])(=[OX1])(=[OX1])[#6]),$([#16X4+2]([NX3])([OX1-])([OX1-])[#6])]',
+    'ester': '[$([CX3](=O)[OX2H0][#6])]',
+    'sulfonate ester': '[$([#16X4](=[OX1])(=[OX1])([#6])[OX2H0]),$([#16X4+2]([OX1-])([OX1-])([#6])[OX2H0])]',
+    'sulfonic acid': '[$([#16X4](=[OX1])(=[OX1])([#6])[OX2H,OX1H0-]),$([#16X4+2]([OX1-])([OX1-])([#6])[OX2H,OX1H0-])]',
+    'carboxylic acid': '[$([CX3](=O)[OH]);!$([CX3](=[OX1])([OX1-])[$([OX2H]),$([OX1-])])]',
+    'nitrogen': '[NX3;!$(NC=O);!$(NS=O);!$(N=O);!$(N=S);!$(NC=N)]',
+    'hydroxyl': '[#6;!$(C=O)][OX2H]',
+    'alpha hydrogen': '[$([CX3](=[OX1])[CH]),$([CX3](=[OX1])[CH2]),$([CX3](=[OX1])[CH3]),$([#16](=[OX1])[CH]),$([#16](=[OX1])[CH2]),$([#16](=[OX1])[CH3]);!$([CX3](=O)[OH]);!$([CX3](=O)[O-]);!$([#6](=[OX1])([CX3]=[CX3]))]',
+    'acyl halide': '[$([CX3](=[OX1])[F,Cl,Br,I]),$([#16](=[OX1])(=[OX1])[F,Cl,Br,I])]',
+    'conjugated bond': '[#6](=[OX1])([CX3]=[CX3])',
+    'imine': '[$([CX3]([#6])[#6]),$([CX3H][#6])]=[$([NX2][#6]),$([NX2H])]',
+    #'peroxide': '[OX2,OX1-][OX2,OX1-]',
+    'hydroxyl': '[C;!$(C=O)][OX2H]',
+    'nitrate': '[$([OX1]=[NX3](=[OX1])[OX1-]),$([OX1]=[NX3+]([OX1-])[OX1-])]',
+    'nitrite': '[NX2](=[OX1])[O;$([X2]),$([X1-])]',
+    'reduced sugar': '[OX2;$([r5]1@C(!@[OX2H1])@C@C@C1),$([r6]1@C(!@[OX2H1])@C@C@C@C1)]',
+    'cyanide': '[C-]#N',
+    'guanidino': '[$(N(C)(C)N),$(NC(=N)N)]',
+    'acidic group': '[$([#16X4](=[OX1])(=[OX1])([OX2H,OX1H0-])[OX2H]),$([!H0;N+,F,Cl,Br,I]),$([OH]-[!#6;!#13;!#16]=[!#6]);!$([P](=[OX1])([OX1-])([OX1-])[$([OX2H]),$([OX1-])])]',
+    'basic group': '[$([CX3](=[OX1])([OX1-])[$([OX2H]),$([OX1-])]),$([CX3](=O)[O-]),$([SX3](=[OX1])([OX1-])[$([OX2H]),$([OX1-])]),$([P](=[OX1])([OX1-])([OX1-])[$([OX2H]),$([OX1-])]),$([O-2]),$([OH-]),$([Si](=[OX1])([OX1-])[OX1-]),$([Ca,Mg](=[OX1])),$([N])]',
+    'metal ion': '[$([Mg+2]),$([Ca+2]),$([Zn+2]),$([Cu+2]),$([Al+3]),$([Fe+2]),$([Fe+3]),$([Mg]*),$([Ca]*),$([Zn]*),$([Cu]*),$([Al]*),$([Fe]*),$([Mg]=*),$([Ca]=*),$([Zn]=*),$([Cu]=*),$([Al]=*),$([Fe]=*)]',
+}
 
 #%%
 def get_cid(api, option):
@@ -22,6 +81,15 @@ def get_cid(api, option):
     elif option == 'SMILES':
         compound = pcp.get_compounds(api, 'smiles')[0]
     return int(compound.cid)
+
+def get_smiles(api, option):
+    if option == 'Name':
+        compound = pcp.get_compounds(api, 'name')[0]
+    elif option == 'PubChem CID':
+        compound = pcp.Compound.from_cid(int(api))
+    elif option == 'SMILES':
+        compound = pcp.get_compounds(api, 'smiles')[0]
+    return compound.isomeric_smiles
 
 #%%
 def getMolDescriptors(mol, missingVal=None):
@@ -45,11 +113,28 @@ with col1:
     option1 = st.selectbox('Search Option', ['Name', 'PubChem CID', 'SMILES'])
 with col2:
     API_CID = st.text_input('Enter name, Pubchem CID or smiles string of the API')
+    if st.button('Check API Structure'):
+        API_Structure = get_smiles(API_CID, option1)
+        m = Chem.MolFromSmiles(API_Structure)
+        if m is None:
+            st.error("Invalided SMILES!")
+        else:
+            API_img = Draw.MolToImage(m, size=(800,400))
+            st.image(API_img, caption="API Structure", use_column_width=True)
+            
 col3, col4 = st.columns([1,3])
 with col3: 
     option3 = st.selectbox('', ['Name', 'PubChem CID', 'SMILES'])
 with col4:
     Excipient_CID = st.text_input('Enter name, Pubchem CID or smiles string of the excipient')
+    if st.button('Check Excipient Structure'):
+        Excipient_Structure = get_smiles(Excipient_CID, option3)
+        n = Chem.MolFromSmiles(Excipient_Structure)
+        if n is None:
+            st.error("Invalided SMILES!")
+        else:
+            Excipient_img = Draw.MolToImage(n, size=(800,400))
+            st.image(Excipient_img, caption="Excipient Structure", use_column_width=True)
 
 df1 = pd.read_csv('data.csv')
 #%%
@@ -68,18 +153,84 @@ if st.button('Result'):
         outcome1 = longle1.loc[:, 'Outcome1']
         if outcome1.iloc[0] == 1:
             Predict_Result1 = f'Incompatible. Probality: {random.uniform(95.00, 100.00):.2f}%'
+            st.success(Predict_Result1)
+            Excipient = pcp.Compound.from_cid(Excipient_CID)
+            Excipient_Structure = Excipient.isomeric_smiles
+            API = pcp.Compound.from_cid(API_CID)
+            API_Structure = API.isomeric_smiles
+            df=pd.read_csv('mechanism.csv')
+
+            # Call the identify_functional_groups function for both compounds
+            functional_groups_API = identify_functional_groups(API_Structure, functional_groups)
+            functional_groups_exp = identify_functional_groups(Excipient_Structure, functional_groups)
+
+            # Create a set of functional groups for faster comparison
+            FG_API = set(functional_groups_API)
+            FG_exp = set(functional_groups_exp)
+            MoI = []
+            MoI_index = 0
+            API_fg = []
+            exp_fg = []
+            for index, row in df.iterrows():
+                if row.iloc[1] in FG_API and row.iloc[2] in FG_exp:
+                    MoI.append(row.iloc[0])
+                    API_fg.append(row.iloc[1])
+                    exp_fg.append(row.iloc[2])
+                    MoI_index = MoI_index + 1
+
+            if MoI == []:
+                st.success('Proposed Mechanism: Unknown type of interaction')
+            else:
+                unique_MoI = list(set(MoI))
+                st.success(f'Proposed Mechanism: {", ".join(unique_MoI)}')
+                st.success('Explanation:')
+                for i in range(MoI_index):
+                    st.success(f'{i+1}. The drug contains {API_fg[i]} while the excipient contains {exp_fg[i]}, enabling them to interact via {MoI[i]}')
         else:
             Predict_Result1 = f'Compatible. Probality: {random.uniform(95.00, 100.00):.2f}%'
-        st.success(Predict_Result1)
+            st.success(Predict_Result1)
         st.success('Please note that the result presented is based solely on the prediction of the model. Therefore, further validation experiments are necessary to confirm the accuracy of the prediction.')
 
     elif not longle2.empty:
         outcome2 = longle2.loc[:, 'Outcome1']
         if outcome2.iloc[0] == 1:
              Predict_Result2 = f'Incompatible. Probality: {random.uniform(95.00, 100.00):.2f}%'
+             st.success(Predict_Result2)
+             Excipient = pcp.Compound.from_cid(Excipient_CID)
+             Excipient_Structure = Excipient.isomeric_smiles
+             API = pcp.Compound.from_cid(API_CID)
+             API_Structure = API.isomeric_smiles
+             df=pd.read_csv('mechanism.csv')
+
+             # Call the identify_functional_groups function for both compounds
+             functional_groups_API = identify_functional_groups(API_Structure, functional_groups)
+             functional_groups_exp = identify_functional_groups(Excipient_Structure, functional_groups)
+
+             # Create a set of functional groups for faster comparison
+             FG_API = set(functional_groups_API)
+             FG_exp = set(functional_groups_exp)
+             MoI = []
+             MoI_index = 0
+             API_fg = []
+             exp_fg = []
+             for index, row in df.iterrows():
+                if row.iloc[1] in FG_API and row.iloc[2] in FG_exp:
+                    MoI.append(row.iloc[0])
+                    API_fg.append(row.iloc[1])
+                    exp_fg.append(row.iloc[2])
+                    MoI_index = MoI_index + 1
+
+             if MoI == []:
+                st.success('Proposed Mechanism: Unknown type of interaction')
+             else:
+                unique_MoI = list(set(MoI))
+                st.success(f'Proposed Mechanism: {", ".join(unique_MoI)}')
+                st.success('Explanation:')
+                for i in range(MoI_index):
+                    st.success(f'{i+1}. The drug contains {API_fg[i]} while the excipient contains {exp_fg[i]}, enabling them to interact via {MoI[i]}')
         else:
              Predict_Result2 = f'Compatible. Probality: {random.uniform(95.00, 100.00):.2f}%'
-        st.success(Predict_Result2)
+             st.success(Predict_Result2)
         st.success('Please note that the result presented is based solely on the prediction of the model. Therefore, further validation experiments are necessary to confirm the accuracy of the prediction.')
         
     else:   
@@ -142,13 +293,34 @@ if st.button('Result'):
         else:
             Predict_Result3 = f'Compatible. Probality: {probs0[0]}%'
         st.success(Predict_Result3)
-        st.success('Please note that the result presented is based solely on the prediction of the model. Therefore, further validation experiments are necessary to confirm the accuracy of the prediction.')
+        if y_prediction[0] == 1:
+            #%% Propose mechanism
+            df=pd.read_csv('mechanism.csv')
 
-st.markdown(
-    """
-    <div style="position: fixed; bottom: 8px; width: 100%; text-align: left; padding-left: 5cm;">
-        For the updated version of this website, please visit <a href="https://decompatibility-v2.streamlit.app">here</a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+            # Call the identify_functional_groups function for both compounds
+            functional_groups_API = identify_functional_groups(API_Structure, functional_groups)
+            functional_groups_exp = identify_functional_groups(Excipient_Structure, functional_groups)
+
+            # Create a set of functional groups for faster comparison
+            FG_API = set(functional_groups_API)
+            FG_exp = set(functional_groups_exp)
+            MoI = []
+            MoI_index = 0
+            API_fg = []
+            exp_fg = []
+            for index, row in df.iterrows():
+                if row.iloc[1] in FG_API and row.iloc[2] in FG_exp:
+                    MoI.append(row.iloc[0])
+                    API_fg.append(row.iloc[1])
+                    exp_fg.append(row.iloc[2])
+                    MoI_index = MoI_index + 1
+
+            if MoI == []:
+                st.success('Proposed Mechanism: Unknown type of interaction')
+            else:
+                unique_MoI = list(set(MoI))
+                st.success(f'Proposed Mechanism: {", ".join(unique_MoI)}')
+                st.success('Explanation:')
+                for i in range(MoI_index):
+                    st.success(f'{i+1}. The drug contains {API_fg[i]} while the excipient contains {exp_fg[i]}, enabling them to interact via {MoI[i]}')
+        st.success('Please note that the result presented is based solely on the prediction of the model. Therefore, further validation experiments are necessary to confirm the accuracy of the prediction.')
